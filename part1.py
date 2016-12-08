181fec67877f7cf6221d8f0d1d52a156cae3b63e
segis95 <segis95@mail.ru>
2016-12-08 16:20:52 +0100

    фы

--- part1.py
+++ part1.py
@@ -38,7 +38,7 @@ cbar.add_lines(CS2)
 #*******************GRADIENT DESCENT*****************
 x = 1.0
 y = 0.0
-ro = 0.0045
+ro = 0.002
 plt.plot([1.0],[0.0],c = 'black', marker = 'o', markersize=10)
 i = 0
 #(1.0,0.0)
@@ -47,10 +47,10 @@ while((x - 1.0)**2 + 100.0 * (x**2 - y)**2 > 1e-3):
    x = x - ro * (2 * (x - 1) + 400 * (x**3 - x * y))
    y = y - ro * (100 * (y - x**2))
    i = i + 1
-   print(x,y)
+   #print(x,y)
    if (i == 1):
        ax.quiver([1.0],[0.0],[-0.45],[1.0],angles='xy',scale = 0.94)
-       print(x,y)
+
    #if (i % 100 == 0):
 
    plt.plot(x, y, c = 'r', marker = 'o', markersize=2)
